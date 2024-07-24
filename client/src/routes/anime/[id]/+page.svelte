9a313dcd41139babc70431334ffd4c76ce1a701d
Diego Chan <diego.chan@utp.ac.pa>
2024-07-24 00:30:38 -0500

    Add anime information
    - Anime page can now display star-rating, overall review number, and action buttons
    
    - Added viewport responsiveness

--- client/src/routes/anime/[id]/+page.svelte
+++ client/src/routes/anime/[id]/+page.svelte
@@ -1,38 +1,7 @@
 <script lang="ts">
+    import HeroBanner from "$lib/HeroBanner.svelte";
     import StarRating from "$lib/StarRating.svelte";
-
-    type Source = {
-        media?: string;
-        src: string;
-        mime: string;
-    };
-
-    type Track = {
-        kind: string;
-        src: string;
-        lang: string;
-        label: string;
-    };
-
-    type Video = {
-        srcset: Source[];
-        tracks: Track[];
-        title?: string;
-        poster?: string;
-    }; 
-
-    type Picture = {
-        srcset: Source[];
-        title?: string;
-    };
-
-    type Anime = {
-        video?: Video;
-        image: Picture;
-        name: string,
-        seasons: number;
-        episodes: number;
-    };
+    import type { Source, Track, Video, Picture, Anime } from "$lib/types.ts";
 
     export const anime: Anime = {
         video: {
@@ -50,39 +19,36 @@
     }
 </script>
 
-<section id="hero-banner">
-    <div id="banner-container" class="w-full bg-gradient-to-r from-black  via-neutral-800 via-50% to-black flex justify-center">
-        <div id="banner-wrapper" class="w-1/2 aspect-video h-auto">
-            {#if anime.video} 
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video title={anime.video.title} class="w-full h-full" controls>
-                {#each anime.video.srcset as source}
-                <source media={source.media} src={source.src} type={source.mime}/>
-                {/each}
-                {#each anime.video.tracks as track}
-                <track kind={track.kind} src={track.src} srclang={track.lang} label={track.label}/>
-                {/each}
-            </video>
-            {:else} 
-            <picture title={anime.image.title} class="w-full h-full overflow-hidden">
-                {#each anime.image.srcset as source}
-                <source media={source.media} srcset={source.src} type={source.mime}/>
-                {/each}
-                <img src={anime.image.srcset[0].src} alt="fallback" class="w-full h-full object-cover">
-            </picture>
-            {/if}
-        </div>
-    </div>
-</section>
-<main id="media-information" class="container mx-auto px-8">
+<HeroBanner />
+<main id="media-information" class="container mx-auto px-3 md:px-8">
     <section id="main-info" class="my-8">
         <h1 id="media-name" class="text-3xl font-semibold my-4">{anime.name}</h1>
-        <div id="seasons-episodes" class="flex items-center gap-5 text-sm text-slate-400">
+        <div id="seasons-episodes" class="flex items-center gap-3 text-sm text-slate-400">
             <span id="season-count">{anime.seasons} Seasons</span>
+            |
             <span id="episode-count">{anime.episodes} Episodes (on average)</span>
         </div>
         <div id="star-ratings">
-            <StarRating />
+            <StarRating size="md"/>
+        </div>
+        <div id="action-buttons" class="my-6 text-sm md:text-md flex w-full md:w-1/2 xl:w-1/3 gap-5">
+            <button class="button bg-blue-500 hover:bg-blue-600 active:bg-blue-700" id="list">
+                <i class="icon bi bi-plus"></i>
+                <span>ADD TO LIST</span>
+            </button>
+            <button class="button bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700" id="list">
+                <i class="icon bi bi-filetype-php"></i>
+                <span>explode()</span>
+            </button>
         </div>
     </section>
 </main>
+
+<style>
+    .icon {
+        @apply flex items-center justify-center;
+    }
+    .button {
+        @apply flex flex-1 items-center justify-center gap-1 p-2 rounded-lg text-lg duration-150;
+    }
+</style>
