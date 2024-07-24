ce8ff35d38ba845d165a3575fad27d8a557d34a3
Jonathan Salazar <jonyalex1047@gmail.com>
2024-07-23 23:55:28 -0500

    soy un maestro de los media queries bro (responsive del hero banner)

--- client/src/routes/anime/[id]/+page.svelte
+++ client/src/routes/anime/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import StarRating from "$lib/StarRating.svelte";
+    import HeroBanner from "$lib/HeroBanner.svelte";
 
     type Source = {
         media?: string;
@@ -35,11 +36,11 @@
     };
 
     export const anime: Anime = {
-        video: {
-            srcset: [ { src: "/video/video.mp4", mime: "video/mp4" } ],
-            tracks: [ { kind: "captions", src: "static/track/captions.vtt", lang: "en", label: "English" } ],
-            title: "My Hero Academia trailer"
-        },
+        // video: {
+        //     srcset: [ { src: "/video/video.mp4", mime: "video/mp4" } ],
+        //     tracks: [ { kind: "captions", src: "static/track/captions.vtt", lang: "en", label: "English" } ],
+        //     title: "My Hero Academia trailer"
+        // },
         image: {
             srcset: [ {media: "(min-width: 0px;)", src: "/image/image.png", mime: "image/png" } ],
             title: "My Hero Academia poster"
@@ -49,34 +50,10 @@
         episodes: 26,
     }
 </script>
-
-<section id="hero-banner">
-    <div id="banner-container" class="w-full bg-gradient-to-r from-black  via-neutral-800 via-50% to-black flex justify-center">
-        <div id="banner-wrapper" class="w-1/2 aspect-video h-auto">
-            {#if anime.video} 
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video title={anime.video.title} class="w-full h-full" controls>
-                {#each anime.video.srcset as source}
-                <source media={source.media} src={source.src} type={source.mime}/>
-                {/each}
-                {#each anime.video.tracks as track}
-                <track kind={track.kind} src={track.src} srclang={track.lang} label={track.label}/>
-                {/each}
-            </video>
-            {:else} 
-            <picture title={anime.image.title} class="w-full h-full overflow-hidden">
-                {#each anime.image.srcset as source}
-                <source media={source.media} srcset={source.src} type={source.mime}/>
-                {/each}
-                <img src={anime.image.srcset[0].src} alt="fallback" class="w-full h-full object-cover">
-            </picture>
-            {/if}
-        </div>
-    </div>
-</section>
-<main id="media-information" class="container mx-auto px-8">
-    <section id="main-info" class="my-8">
-        <h1 id="media-name" class="text-3xl font-semibold my-4">{anime.name}</h1>
+<HeroBanner />
+<main id="media-information" class="container mx-auto px-2 md:px-8">
+    <section id="main-info" class="my-5 md:my-8">
+        <h1 id="media-name" class="text-2xl md:text-3xl font-semibold my-4">{anime.name}</h1>
         <div id="seasons-episodes" class="flex items-center gap-5 text-sm text-slate-400">
             <span id="season-count">{anime.seasons} Seasons</span>
             <span id="episode-count">{anime.episodes} Episodes (on average)</span>
